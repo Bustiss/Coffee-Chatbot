6b4bbaf8e44899b81c241ae64f79b26b36758e90
Bustiss <williejones321@gmail.com>
2024-12-05 03:08:33 -0600

    added get_drink_type functionality

--- script.py
+++ script.py
@@ -1,8 +1,12 @@
-# Define your functions
+# Description: This script is a simple program that simulates a coffee bot at a cafe. 
 def coffee_bot():
   print("Welcome to the Cafe!")
+  size = get_size()
+  print(size)
+  drink_type = get_drink_type()
+  print(drink_type)
 
-# Ask user to input preferred cup size
+# Get the size of the drink
 def get_size():
   res = input('What size drink can I get for you? \n[a] Small \n[b] Medium \n[c] Large \n> ')
   
@@ -14,11 +18,25 @@ def get_size():
     return 'large'
   else:
     print_message()
-    return get_size() # if the user enters an invalid response, run the function again
+    return get_size()
+
+# Get the type of drink
+def get_drink_type():
+  res = input('What type of drink would you like? \n[a] Brewed Coffee \n[b] Mocha \n[c] Latte \n>')
   
+  if res == 'a':
+    return 'Brewed Coffee'
+  elif res == 'b':
+    return 'Mocha'
+  elif res == 'c':
+    return 'Latte'
+  else:
+    print_message()
+    return get_drink_type() 
+  
+# Print a message for the user
 def print_message():
   print("I'm sorry, I did not understand your selection. Please enter the corresponding letter for your response.")
 
 # Call coffee_bot()!
 coffee_bot()
-get_size()
