1b06a1f1ef8be85017e03b0cf623f9279ae4cb74
Bustiss <williejones321@gmail.com>
2024-12-05 03:32:38 -0600

    added functionality for cup type, hot or iced, and output message when coffeeBot is called

--- script.py
+++ script.py
@@ -2,9 +2,12 @@
 def coffee_bot():
   print("Welcome to the Cafe!")
   size = get_size()
-  print(size)
+  temp = hot_or_iced()
   drink_type = get_drink_type()
-  print(drink_type)
+  cup = choose_cup()
+  print(f"Alright, that's a {size} {temp} {drink_type} in a {cup}")
+  name = input('Can I get your name please? ')
+  print(f"Thanks, {name}! Your drink will be ready shortly.")
 
 # Get the size of the drink
 def get_size():
@@ -29,14 +32,52 @@ def get_drink_type():
   elif res == 'b':
     return 'Mocha'
   elif res == 'c':
-    return 'Latte'
+    return order_latte()
   else:
     print_message()
-    return get_drink_type() 
-  
-# Print a message for the user
+    return get_drink_type()
+
+# If the user selects a latte, ask for the type of milk
+def order_latte():
+  res = input('And what kind of milk for your latte? \n[a] 2% milk \n[b] Non-fat milk \n[c] Soy milk \n>')
+
+  if res == 'a':
+    return 'latte'
+  elif res == 'b':
+    return 'non-fat latte'
+  elif res == 'c':
+    return 'soy latte'
+  else:
+    print_message()
+    return order_latte() 
+
+# Ask the user if they want a plastic cup or a reuseable cup
+def choose_cup():
+  res = input('Would you like plastic cup or your own reuseablecup? \n[a] Plastic cup \n[b] reuseable cup \n>')
+
+  if res == 'a':
+    return 'plastic cup'
+  elif res == 'b':
+    return 'reuseable cup'
+  else:
+    print_message()
+    return choose_cup()
+
+# Ask the user if they want their drink hot or iced
+def hot_or_iced():
+  res = input('Would you like your drink hot or iced? \n[a] Hot \n[b] Iced \n>')
+
+  if res == 'a':
+    return 'hot'
+  elif res == 'b':
+    return 'iced'
+  else:
+    print_message()
+    return hot_or_iced()
+
+# Print a message if the user enters an invalid response
 def print_message():
   print("I'm sorry, I did not understand your selection. Please enter the corresponding letter for your response.")
 
-# Call coffee_bot()!
+# Call coffee_bot()
 coffee_bot()
